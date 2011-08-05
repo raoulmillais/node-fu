890f677c4b820856884fe864ed9bfbf826e5f03b
Raoul Millais <raoul@raoulmillais.com>
2011-08-05 17:35:15 +0100

    Added dependency versions to fix invalid package.json and started implementing configure-package.js

--- configure-package.js
+++ configure-package.js
@@ -10,11 +10,29 @@ set author - git config --global github.user  && git config --global user.name
 
 */
 
-var args = process.argv,
+var fs = require('fs'),
+	args = process.argv,
 	i, l;
+
+args.shift(); // node
+args.shift(); // this script
 
 for (i = 0, l = args.length; i < l; i++) {
 	console.log(args[i]);
 }
 
+if (args.length != 4) {
+	throw new Error('Incorrect number of arguments, expected github username' +
+					', git username, git email and path to package.json');
+}
 
+fs.readFile(args[3], function(err, data) {
+	var parsed;
+
+	if(err) {
+		throw new Error(err);
+	}
+	
+	parsed = JSON.parse(data);
+	//parsed.name = 
+});
