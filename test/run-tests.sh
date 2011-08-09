000246857a8137de507beb638bad020dbd17654c
Raoul Millais <raoul@raoulmillais.com>
2011-08-09 09:03:55 +0100

    Added some tests round the fu script

---
+++ test/run-tests.sh
@@ -0,0 +1,43 @@
+#! /usr/bin/env roundup
+
+describe "When creating projects in an existing folder"
+
+before() {
+	mkdir ../../test-project
+	../fu project ../../test-project/
+	cd ../../test-project
+}
+
+after() {
+	cd -
+	rm -rf ../../test-project
+}
+
+it_creates_a_git_repo() {
+	status=$(git status > /dev/null ; echo $?)
+	test 0 -eq $status
+}
+
+it_creates_the_project_structure() {
+	test -d lib
+	test -d bin
+	test -d support
+	test -d spec
+}
+
+it_copies_a_readyjs_config() {
+	test -r readyjs.conf
+}
+
+it_copies_a_package_definition() {
+	test -r package.json
+}
+
+it_copies_a_makefile() {
+	test -r Makefile
+}
+
+it_copies_the_versioning_script() {
+	test -x bump-version.sh
+}
+
